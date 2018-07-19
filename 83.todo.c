352eb773ea58d4e0e23e6ab71ca15cb0e15e215c
Deron.W <w.del@qq.com>
2018-07-19 21:18:52 +0800

    I'm too lazy, nothing left

---
+++ 83.todo.c
@@ -0,0 +1,13 @@
+struct ListNode{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode * deleteDuplicates(struct ListNode *head){
+    return head;
+}
+
+int main(){
+    List
+    return 0;
+}
